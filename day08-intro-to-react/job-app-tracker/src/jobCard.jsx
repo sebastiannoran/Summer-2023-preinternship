de65386146cb7e7fcbca86ee49cc3ffd60928f38
sebastiannoran <119528708+sebastiannoran@users.noreply.github.com>
2023-06-22 13:28:36 -0400

    comitt

---
+++ day08-intro-to-react/job-app-tracker/src/jobCard.jsx
@@ -0,0 +1,22 @@
+function jobCard (job) {
+    return {
+        <div className = "j-desc">
+        <img
+    class="j-desc__company-image"
+  />
+  <div class="j-desc__details">
+    <h2 class="j-desc__job-title">
+
+    </h2>
+    <p class="j-desc__company"></p>
+    <ul class="j-desc__metadata">
+      <li class="j-desc__location"></li>
+      <li class="j-desc__salary"></li>
+      <li class="j-desc__posting_date"></li>
+    </ul>
+    </div>
+  </div>
+    }
+}
+
+export default jobCard;
