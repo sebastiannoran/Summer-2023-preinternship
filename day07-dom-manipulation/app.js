46dde2960ee8e0ab9673ef5147ae2a6b6cc0a7d8
sebastiannoran <119528708+sebastiannoran@users.noreply.github.com>
2023-06-22 10:04:33 -0400

    commit

--- day07-dom-manipulation/app.js
+++ day07-dom-manipulation/app.js
@@ -67,14 +67,14 @@ const jobs = [
   },
 ];
 
-console.log(document.body);
+// console.log(document.body);
 
 // getElementById does not require the # symbol to access elements in DOM
-console.log(document.getElementById('jobs'));
-console.log(document.querySelector('#jobs'));
+// console.log(document.getElementById('jobs'));
+// console.log(document.querySelector('#jobs'));
 
-console.log(document.getElementById('jobs').id);
-console.log(document.getElementById('jobs').textContent);
+// console.log(document.getElementById('jobs').id);
+// console.log(document.getElementById('jobs').textContent);
 
 // let jobDiv = document.querySelectorAll('.j-desc');
 
@@ -125,9 +125,93 @@ console.log(document.getElementById('jobs').textContent);
 //     debugger;
 // }
 
-createNewJob();function createNewJob(job) {
-  const jobDiv = document.createElement('div');
-  jobDiv.className = 'j-desc';
+// createNewJob();function createNewJob(job) {
+//   const jobDiv = document.createElement('div');
+//   jobDiv.className = 'j-desc';
+//   jobDiv.innerHTML = `
+//   <img
+//     class="j-desc__company-image"
+//   />
+//   <div class="j-desc__details">
+//     <h2 class="j-desc__job-title">
+      
+//     </h2>
+//     <p class="j-desc__company"></p>
+//     <ul class="j-desc__metadata">
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//     </ul>
+//   </div>
+//   `;
+
+//   // pull data from the arguments
+//   const { image: {src, alt}, company, title, salary, location, postDate} = job;
+
+//   // populate data into DOM
+//   const img = jobDiv.querySelector(".j-desc__company-image");
+//   img.setAttribute("src", src);
+//   img.setAttribute("alt", alt);
+//   jobDiv.querySelector(".j-desc__job-title").textContent = title;
+//   jobDiv.querySelector(".j-desc__company").textContent = company;
+//   jobDiv.querySelector(".j-desc__location").textContent = location;
+//   jobDiv.querySelector(".j-desc__salary").textContent = salary;
+//   jobDiv.querySelector(".j-desc__post-date").textContent = postDate;
+
+
+//   document.querySelector('job').appendChild(jobDiv);
+// }
+
+// createNewJob(jobs[3]);
+
+
+
+
+// console.log(document);
+// console.log(document.body);
+console.dir(document.body);
+
+// console.log(document.getElementById("jobs"))
+// console.log(document.querySelector("#jobs"))
+// console.log(document.querySelector("#jobs").id)
+// console.log(document.querySelector("#jobs").textContent)
+
+// console.log(document.getElementsByClassName("j-desc"));
+// console.log(Array.from(document.getElementsByClassName("j-desc")).constructor);
+
+// const jobDivs = Array.from(document.getElementsByClassName('j-desc'));
+const jobDivs = document.querySelectorAll('.j-desc');
+
+const firstJob = jobDivs[0];
+// console.log(firstJob);
+// console.log(firstJob.children);
+// console.log(firstJob.parentElement);
+
+const secondJob = firstJob.nextElementSibling;
+// console.log(secondJob);
+// console.log(firstJob === secondJob.previousElementSibling);
+
+document.querySelectorAll(".j-desc").forEach((jobDiv) => {
+  const jobTitle = jobDiv.querySelector(".j-desc__job-title").textContent;
+  console.log(jobTitle);
+});
+
+const firstJobImage = firstJob.querySelector(".j-desc__company-image");
+console.log(firstJobImage.alt);
+firstJobImage.setAttribute("alt", "Patterned Learning AI - Empowering with AI!")
+console.log(firstJobImage.alt);
+
+const firstJobTitle = firstJob.querySelector('.j-desc__job-title');
+console.log(firstJobTitle.textContent);
+firstJobTitle.textContent = "Front-end Developer"
+// firstJobTitle.style.color = "blue";
+// firstJobTitle.style.textAlign = "center";
+console.log(firstJobTitle.classList);
+
+function createNewJob(job) {
+  const jobDiv = document.createElement("div");
+  jobDiv.className = "j-desc";
+  // create structure
   jobDiv.innerHTML = `
   <img
     class="j-desc__company-image"
@@ -138,17 +222,17 @@ createNewJob();function createNewJob(job) {
     </h2>
     <p class="j-desc__company"></p>
     <ul class="j-desc__metadata">
-      <li></li>
-      <li></li>
-      <li></li>
+      <li class="j-desc__location"></li>
+      <li class="j-desc__salary"></li>
+      <li class="j-desc__posting_date"></li>
     </ul>
   </div>
   `;
 
-  // pull data from the arguments
-  const { image: {src, alt}, company, title, salary, location, postDate} = job;
-
-  // populate data into DOM
+  // pull data from argument
+  const { image: { src, alt }, company, title, salary, location, postDate } = job;
+  
+  // populate data into DOM structure
   const img = jobDiv.querySelector(".j-desc__company-image");
   img.setAttribute("src", src);
   img.setAttribute("alt", alt);
@@ -156,12 +240,50 @@ createNewJob();function createNewJob(job) {
   jobDiv.querySelector(".j-desc__company").textContent = company;
   jobDiv.querySelector(".j-desc__location").textContent = location;
   jobDiv.querySelector(".j-desc__salary").textContent = salary;
-  jobDiv.querySelector(".j-desc__post-date").textContent = postDate;
-
-
-  document.querySelector('job').appendChild(jobDiv);
+  jobDiv.querySelector(".j-desc__posting_date").textContent = postDate;
+  
+  // add element to the DOM
+  document.querySelector('#jobs').append(jobDiv);
 }
 
 createNewJob(jobs[3]);
+createNewJob(jobs[4]);
+createNewJob(jobs[5]);
 
+const jobTitles = document.getElementsByClassName("j-desc__job-title");
 
+for (let i = 0; i < jobTitles.length; i++) {
+  jobTitles[i].onclick = function () {
+    alert("You clicked on " + this.textContent);
+  };
+  jobTitles[i].onmouseover = function () {
+    this.style.color = "red";
+  };
+  jobTitles[i].onmouseout = function () {
+    this.style.color = "";
+  };
+}
+
+// function handleTitleClick(event) {
+//   console.log('you clicked!')
+// }
+
+// document.addEventListener("submit", (event) => {
+//   if (event.target.matches("#newJob")) {
+//     handleNewJob(event);
+//   } else if (event.target.matches(".newComment")) {
+//     handleNewComment(event);
+//   }
+// });
+
+const form = document.getElementById("jobForm");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault(); // prevent browser default behavior
+
+  const jobTitle = document.getElementById("jobTitle").value;
+  const companyName = document.getElementById("companyName").value;
+
+  console.log("Job Title: " + jobTitle);
+  console.log("Company Name: " + companyName);
+});
