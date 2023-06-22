1c176d54782b135aa23c40bbd7d752194f597ecb
sebastiannoran <119528708+sebastiannoran@users.noreply.github.com>
2023-06-22 13:38:00 -0400

    Merge branch 'main' of https://github.com/CSI-Tech-Talent-Pipeline/Summer-2023-preinternship

--- day08-intro-to-react/job-app-tracker/src/jobCard.jsx
+++ day08-intro-to-react/job-app-tracker/src/jobCard.jsx
@@ -1,22 +1,32 @@
-function jobCard (job) {
-    return {
-        <div className = "j-desc">
+function JobCard({job}) {
+    console.log(job);
+    const {
+      image: { src, alt },
+      company,
+      title,
+      salary,
+      location,
+      postDate,
+    } = job;
+  
+    return (
+      <div className="j-desc">
         <img
-    class="j-desc__company-image"
-  />
-  <div class="j-desc__details">
-    <h2 class="j-desc__job-title">
-
-    </h2>
-    <p class="j-desc__company"></p>
-    <ul class="j-desc__metadata">
-      <li class="j-desc__location"></li>
-      <li class="j-desc__salary"></li>
-      <li class="j-desc__posting_date"></li>
-    </ul>
-    </div>
-  </div>
-    }
-}
-
-export default jobCard;
+          className="j-desc__company-image"
+          src={src}
+          alt={alt}
+        />
+        <div className="j-desc__details">
+          <h2 className="j-desc__job-title">{title}</h2>
+          <p className="j-desc__company">{company}</p>
+          <ul className="j-desc__metadata">
+            <li className="j-desc__location">{location}</li>
+            <li className="j-desc__salary">{salary}</li>
+            <li className="j-desc__posting_date">{postDate}</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+  
+  export default JobCard;
